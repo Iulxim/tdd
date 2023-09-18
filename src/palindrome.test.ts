a9133b5bd598f402f0b3796adef82135cffdef5c
Iulian Maxim <iulian@Iulians-MacBook-Pro.local>
2023-09-18 13:23:10 +0300

    test: it(2) -> red

--- src/palindrome.test.ts
+++ src/palindrome.test.ts
@@ -4,4 +4,8 @@ describe('palindrome', () => {
   it("should not have empty string", () => {
     expect(palindrome("")).toBe("Your input should not be an empty string");
   });
+  
+  it("should be reversed", () => {
+    expect(palindrome("Palindrome")).toBe("emordnilaP");
+  });
 });
