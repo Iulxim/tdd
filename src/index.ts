93b68c94c1c7254ef28b4a40335b5079c4eb1256
Iulian Maxim <iulian@Iulians-MacBook-Pro.local>
2023-09-18 15:02:11 +0300

    test: green

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export function palindrome(str: string) {
   if (str === "") {
     return 'Your input should not be an empty string';
   }
-  const lowerCaseString = str.toLowerCase();
+  const lowerCaseString = str.toLowerCase().trim();
 
   return lowerCaseString;
 }
